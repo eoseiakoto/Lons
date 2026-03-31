f2c65478407937b327c49f80784fbb28020a03da
Emmanuel O-A <eoseiakoto@gmail.com>
2026-03-31 19:41:40 +0400

    fix(ci): resolve pnpm version conflict and add missing Python dependencies

--- infrastructure/terraform/modules/dns/outputs.tf
+++ infrastructure/terraform/modules/dns/outputs.tf
@@ -9,7 +9,7 @@ output "name_servers" {
 }
 
 output "certificate_arn" {
-  description = "The ARN of the ACM certificate"
+  description = "The ARN of the validated ACM certificate"
   value       = aws_acm_certificate_validation.main.certificate_arn
 }
 
